c5d7004159dac4f1e128b8f4a32be2813b370113
Santi <2666910480101@ingenieria.usac.edu.gt>
2021-12-28 12:15:13 -0800

    cambios

--- hilos.py
+++ hilos.py
@@ -4,7 +4,6 @@ def print_num(num):
     """
     funcion que imprime numeros del 1 al 10
     """
-    #print("Cube: {}".format(num * num * num))
     for i in range(11):
         print(i)
 
@@ -15,8 +14,8 @@ def print_square(num):
     print("Square: {}".format(num * num))
   
 if __name__ == "__main__":
-    #creando los hilos
+    #creando los hilos y arrancando hilos
     t1 = threading.Thread(target=print_square, args=(10,)).start()
     t2 = threading.Thread(target=print_num, args=(10,)).start()
-
+    
     print("Hilos ejecutados correctamente!")
